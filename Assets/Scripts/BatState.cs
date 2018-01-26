531477b40e063ea90bc5b65a441cfb10cfa4c6e7
ChudeRonczki <lukasz@czyzycki.pl>
2018-01-26 22:56:35 +0100

    Initial gameplay commit

---
+++ Assets/Scripts/BatState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatState : MonoBehaviour
+{
+    public int carriedPoints = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var tran = other.GetComponent<Tran>();
+        if (tran)
+        {
+            carriedPoints += tran.pointsWorth;
+            Destroy(tran.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PotatoMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoMovement : MonoBehaviour
+{
+	void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")) * 10f;
+	}
+}
