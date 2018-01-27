26095b6095baea09ecaf05cf12b67f3b7ffdf199
Ramori <eramori@gmail.com>
2018-01-27 23:20:45 +0100

    Tran container on level 1

--- Assets/Scripts/TranContainer.cs
+++ Assets/Scripts/TranContainer.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class TranContainer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    public float StartScale = 0.05f;
+    public ParticleSystem StateChangeParticles;
+
+    float ScaleStep;
+
+    Vector3 currentScale;
+    float targetScaleY;
+
+    void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        ScaleStep = (1f - StartScale) / Game.Instance.pointsTarget;
+
+        currentScale = new Vector3(1f, StartScale, 1f);
+        targetScaleY = StartScale;
+
+        Game.Instance.StateChanged += OnGameStateChanged;
+    }
+
+    private void OnGameStateChanged()
     {
-		
-	}
+        targetScaleY = Mathf.Min(StartScale + Game.Instance.TotalPointsCollected * ScaleStep, 1f);
+        StateChangeParticles.Play();
+    }
+
+    private void Update()
+    {
+        if (currentScale.y < targetScaleY)
+        {
+            currentScale.y = Mathf.Lerp(currentScale.y, targetScaleY, 0.04f);
+
+            if (targetScaleY - currentScale.y < 0.01f)
+            {
+                currentScale.y = targetScaleY;
+            }
+
+            transform.localScale = currentScale;
+        }
+    }
 }
