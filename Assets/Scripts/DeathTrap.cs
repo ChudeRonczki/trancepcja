ce688ef60b1b6ad642d656b337aa0dd3b7b4cab5
ChudeRonczki <lukasz@czyzycki.pl>
2018-01-27 20:13:54 +0100

    DeathTrap's kill tran by default

--- Assets/Scripts/DeathTrap.cs
+++ Assets/Scripts/DeathTrap.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class DeathTrap : MonoBehaviour
 {
+    public bool destroyTran = true;
+
     private void OnTriggerEnter(Collider other)
     {
         var bat = other.GetComponent<BatRespawner>();
         if (bat)
             bat.Kill();
+
+        var tran = other.GetComponent<Tran>();
+        if (tran && destroyTran)
+            Destroy(tran.gameObject);
     }
 }
